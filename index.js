a355de221cc443dcdbc1d03ee2d32725ba9fdc26
Temuri Takalandze <takalandzet@gmail.com>
2021-12-13 14:19:27 +0400

    ci: add GitHub Actions workflows

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-/*!
+/*
  * This file is part of the ylebich.js.
+ *
  * (c) 2021 Temuri Takalandze <me@abgeo.dev>
+ *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
